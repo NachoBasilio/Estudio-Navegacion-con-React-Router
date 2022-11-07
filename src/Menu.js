508910d540e010f79240d8bd5d2b4bb8c9bc9aee
IgnaDev <arcortoon@gmail.com>
2022-11-07 10:08:56 -0300

    Terminamos de ocultar las cosas del menu

--- src/Menu.js
+++ src/Menu.js
@@ -1,49 +1,62 @@
 import React from 'react'
-import { Link, NavLink } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
+import { useAuth } from './auth'
 
 const routes = []
 routes.push({
   to: '/',
-  text: 'Home'
+  text: 'Home',
+  private: false,
 
 })
 routes.push({
   to: '/blog',
-  text: 'Blog'
+  text: 'Blog',
+  private: false,
 
 })
 routes.push({
   to: '/profile',
   text: 'Profile',
+  private: true,
 
 })
 routes.push({
   to: '/login',
   text: 'Login',
+  publicOnly: true,
+  private: false,
 
 })
 routes.push({
   to: '/logout',
   text: 'Logout',
+  private: true,
 })
 
 export function Menu() {
+  const auth = useAuth()
+
   return (
     <nav>
       <ul>
-        {routes.map(route => (
-          <li key={route.to}>
-            <NavLink   
-            style={({isActive})=>({
-              color: isActive ? 'red' : 'blue'
-            })}
-            to={route.to}
-            
-            >
-              {route.text}
-            </NavLink>
-          </li>
-        ))}
+        {routes.map(route => {
+          if (route.private && !auth.user) return null
+          if (route.publicOnly && auth.user) return null
+          return (
+            <li key={route.to}>
+              <NavLink   
+              style={({isActive})=>({
+                color: isActive ? 'red' : 'blue'
+              })}
+              to={route.to}
+              
+              >
+                {route.text}
+              </NavLink>
+            </li>
+          )
+        })}
       </ul>
     </nav>
   )
